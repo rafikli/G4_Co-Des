7472f559946eab36ac1727c947edcb13b5476751
pedr0luiz <pedroluiz51@gmail.com>
2018-09-21 10:40:56 -0300

    js pricipal

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
           'foto' : data['foto']
       })
       replace('head', {
-          'nome': data['nome']   
+          'nome': data['nome']
       })
     })
   }
@@ -33,7 +33,28 @@ document.addEventListener('DOMContentLoaded', function() {
           'FocoPrático': data['Foco Prático'],
           'FocoTeórico': data['Foco Teórico'],
       })
+      let focoTeorico = document.querySelector('.ver-todos-box1')
+      let focoPratico = document.querySelector('.ver-todos-box2')
+      let focoT = document.querySelector('.foco-tl')
+      let focoP = document.querySelector('.foco-pl')
+      let grupoTeorico = document.querySelector('.grupo-teorico')
+      let grupoPratico = document.querySelector('.grupo-pratico')
+      let itemPratico = document.querySelector('.item-pratico')
+      let itemTeorico = document.querySelector('.item-teorico')
+
+      grupoTeorico.classList.add("sumiu")
+      grupoPratico.classList.add("sumiu")
+      itemTeorico.classList.add("sumiu")
+      itemPratico.classList.add("sumiu")
+
+      focoTeorico.addEventListener("click", function() {
+        console.log('funfaaaa');
+        grupoTeorico.classList.remove("sumiu");
+      })
+      focoPratico.addEventListener('click', function() {
+        console.log('funfaaaa')
+        grupoPratico.classList.remove("sumiu")
+      })
       })
   }
-
 })
