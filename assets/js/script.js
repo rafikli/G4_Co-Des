8316a5d0798d7228b233944e7e1d5c2b918c18eb
pedr0luiz <pedroluiz51@gmail.com>
2018-09-25 18:25:52 -0300

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,28 +41,195 @@ document.addEventListener('DOMContentLoaded', function() {
           'FocoPrático': data['Foco Prático'],
           'FocoTeórico': data['Foco Teórico'],
       })
-      let focoTeorico = document.querySelector('.ver-todos-box1')
-      let focoPratico = document.querySelector('.ver-todos-box2')
+      let foco = document.querySelectorAll('.foco')
+      let focoPratico = foco[1]
+      let focoTeorico = foco[0]
       let focoT = document.querySelector('.foco-tl')
       let focoP = document.querySelector('.foco-pl')
-      let grupoTeorico = document.querySelector('.grupo-teorico')
-      let grupoPratico = document.querySelector('.grupo-pratico')
-      let itemPratico = document.querySelector('.item-pratico')
-      let itemTeorico = document.querySelector('.item-teorico')
+      let grupoTeorico = document.querySelectorAll('.grupo-teorico')
+      let grupoPratico = document.querySelectorAll('.grupo-pratico')
+      let itemPratico = document.querySelectorAll('.item-pratico')
+      let itemTeorico = document.querySelectorAll('.item-teorico')
+      let itemTeoricoEmp = [itemTeorico[0]]
+      let itemTeoricoExa = [itemTeorico[1],itemTeorico[2],itemTeorico[3]]
+      let itemTeoricoPen = [itemTeorico[4]]
+      let itemPraticoAut = [itemPratico[0],itemPratico[1],itemPratico[2]]
+      let itemPraticoDes = [itemPratico[3],itemPratico[4],itemPratico[5]]
+      let itemPraticoPro = [itemPratico[6],itemPratico[7],itemPratico[8]]
 
-      grupoTeorico.classList.add("sumiu")
-      grupoPratico.classList.add("sumiu")
-      itemTeorico.classList.add("sumiu")
-      itemPratico.classList.add("sumiu")
+
+      for(let i of grupoTeorico) {
+        i.classList.add("sumiuvt")
+      }
+      for(let i of grupoPratico) {
+        i.classList.add("sumiuvt")
+      }
+      for(let i of itemTeorico) {
+        i.classList.add("sumiuvt")
+      }
+      for(let i of itemPratico) {
+        i.classList.add("sumiuvt")
+      }
+
+      variFt = 0
+      variFp = 0
+      variGt0 = 0
+      variGt1 = 0
+      variGt2 = 0
+      variGp0 = 0
+      variGp1 = 0
+      variGp2 = 0
 
       focoTeorico.addEventListener("click", function() {
-        console.log('funfaaaa');
-        grupoTeorico.classList.remove("sumiu");
+        if(variFt == 0) {
+          for(let i of grupoTeorico) {
+            i.classList.remove("sumiuvt")
+            this.classList.toggle("active")
+          }
+          variFt = 1
+        }
+        else {
+          for(let i of grupoTeorico) {
+            i.classList.add("sumiuvt")
+            this.classList.toggle("active")
+          }
+          for(let i of itemTeorico) {
+            i.classList.add("sumiuvt")
+          }
+          variFt = 0
+          variGt0 = 0
+          variGt1 = 0
+          variGt2 = 0
+        }
       })
+
       focoPratico.addEventListener('click', function() {
-        console.log('funfaaaa')
-        grupoPratico.classList.remove("sumiu")
+        if(variFp == 0) {
+          for(let i of grupoPratico) {
+            i.classList.remove("sumiuvt")
+            this.classList.toggle("active")
+          }
+          variFp = 1
+        }
+        else {
+          for(let i of grupoPratico) {
+            i.classList.add("sumiuvt")
+            this.classList.toggle("active")
+          }
+          for(let i of itemPratico) {
+            i.classList.add("sumiuvt")
+
+          }
+          variFp = 0
+          variGp0 = 0
+          variGp1 = 0
+          variGp2 = 0
+
+        }
       })
-      })
+      function someT(gT) {
+        gT.addEventListener('click', function(){
+          if(gT == grupoTeorico[0]){
+            if(variGt0 == 0) {
+              for(let i of itemTeoricoEmp) {
+                i.classList.remove("sumiuvt")
+                this.classList.toggle("active")
+              }
+              variGt0 = 1
+            } else {
+              for(let i of itemTeoricoEmp) {
+                i.classList.add("sumiuvt")
+                this.classList.toggle("active")
+              }
+              variGt0 = 0
+            }
+          } else if(gT == grupoTeorico[1]) {
+            if(variGt1 == 0) {
+              for(let i of itemTeoricoExa) {
+                i.classList.remove("sumiuvt")
+                this.classList.toggle("active")
+              }
+              variGt1 = 1
+            } else {
+              for(let i of itemTeoricoExa) {
+                i.classList.add("sumiuvt")
+                this.classList.toggle("active")
+              }
+              variGt1 = 0
+            }
+          } else if(gT == grupoTeorico[2]) {
+            if(variGt2 == 0) {
+              for(let i of itemTeoricoPen) {
+                i.classList.remove("sumiuvt")
+                this.classList.toggle("active")
+              }
+              variGt2 = 1
+            } else {
+              for(let i of itemTeoricoPen) {
+                i.classList.add("sumiuvt")
+                this.classList.toggle("active")
+              }
+              variGt2 = 0
+            }
+          }
+        })
+      }
+      function someP(gP) {
+        gP.addEventListener('click', function(){
+          if(gP == grupoPratico[0]){
+            if(variGp0 == 0) {
+              for(let i of itemPraticoAut) {
+                i.classList.remove("sumiuvt")
+                this.classList.toggle("active")
+              }
+              variGp0 = 1
+            } else {
+              for(let i of itemPraticoAut) {
+                i.classList.add("sumiuvt")
+                this.classList.toggle("active")
+              }
+              variGp0 = 0
+            }
+          } else if(gP == grupoPratico[1]) {
+            if(variGp1 == 0) {
+              for(let i of itemPraticoDes) {
+                i.classList.remove("sumiuvt")
+                this.classList.toggle("active")
+              }
+              variGp1 = 1
+            } else {
+              for(let i of itemPraticoDes) {
+                i.classList.add("sumiuvt")
+                this.classList.toggle("active")
+              }
+              variGp1 = 0
+            }
+          } else if(gP == grupoPratico[2]) {
+            if(variGp2 == 0) {
+              for(let i of itemPraticoPro) {
+                i.classList.remove("sumiuvt")
+                this.classList.toggle("active")
+              }
+              variGp2 = 1
+            } else {
+              for(let i of itemPraticoPro) {
+                i.classList.add("sumiuvt")
+                this.classList.toggle("active")
+              }
+              variGp2 = 0
+            }
+          }
+        })
+      }
+
+      for(let i of grupoPratico) {
+        someP(i)
+      }
+      for(let i of grupoTeorico) {
+        someT(i)
+      }
+
+
+    })
   }
 })
